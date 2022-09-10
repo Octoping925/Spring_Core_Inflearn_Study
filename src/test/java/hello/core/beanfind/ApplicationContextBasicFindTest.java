6faaab1595595d94a89276fe0748fc84317547c3
Octoping <myc1365@naver.com>
2022-09-10 21:35:20 +0900

    스프링 컨테이너와 스프링 빈 - 스프링 빈 조회 - 기본
    스프링 컨테이너에서 스프링 빈을 찾는 가장 기본적인 조회 방법
    
    - ac.getBean(빈이름, 타입)
    - ac.getBean(타입)
    
    조회 대상 스프링 빈이 없으면 `NoSuchBeanDefinitionException` 예외 발생

---
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,44 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextBasicFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertInstanceOf(MemberServiceImpl.class, memberService);
+    }
+
+    @Test
+    @DisplayName("빈 타입으로 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertInstanceOf(MemberServiceImpl.class, memberService);
+    }
+
+
+    @Test
+    @DisplayName("빈 이름으로 조회2")
+    void findBeanByName2() {
+        // 가능은 하지만, 좋은 코드는 아님!
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        Assertions.assertInstanceOf(MemberServiceImpl.class, memberService);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 실패")
+    void findBeanByName_Fail() {
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxx", MemberService.class));
+    }
+}
