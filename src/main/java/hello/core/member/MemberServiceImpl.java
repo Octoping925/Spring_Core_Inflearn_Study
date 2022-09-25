0c55d1273a52690d1c6de9b57113f2bb026830c0
Octoping <myc1365@naver.com>
2022-09-25 19:39:27 +0900

    의존관계 자동 주입 - 롬복과 최신 트렌드
    개발을 해보면 대부분이 다 불변이고, 생성자에 final 키워드를 사용하게 된다.
    
    생성자도 만들고, 주입 받은 값을 대입하는 코드도 만들고 하면 코드 양이 많아진다.
    
    기본 코드를 최적화해보자.
    
    ```java
    @Component
    public class OrderServiceImpl implements OrderService {
    	private final MemberRepository memberRepository;
    	private final DiscountPolicy discountPolicy;
    
    	@Autowired
    	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
    		this.memberRepository = memberRepository;
    		this.discountPolicy = discountPolicy;
    	}
    }
    ```
    
    ```java
    @Component
    @RequiredArgsConstructor
    public class OrderServiceImpl implements OrderService {
        private final MemberRepository memberRepository;
        private final DiscountPolicy discountPolicy;
    }
    ```
    
    @RequiredArgsConstructor를 사용하니 훨씬 코드가 깔끔해졌다.
    
    RequiredArgsContstructor는 final이 붙은 필드들로 생성자를 만들어주는 기능이다.
    
    그리고 생성자가 딱 1개만 있다면 @Autowired를 자동으로 추가해준다.
    
    **정리**
    
    최근에는 생성자를 딱 1개 두고, @Autowired 를 생략하는 방법을 주로 사용한다.
    
    여기에 Lombok 라이브러리의 @RequiredArgsConstructor 함께 사용하면 기능은 다 제공하면서, 코드는 깔끔하게 사용할 수 있다.

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,16 +1,13 @@
 package hello.core.member;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
-    private MemberRepository memberRepository;
-
-    @Autowired
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+    private final MemberRepository memberRepository;
 
     @Override
     public void join(Member member) {
